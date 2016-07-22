8e9ff011f68f51138cba5b8761deda7a4b50777d
Keshav Varma <keshavdv@gmail.com>
2016-07-21 21:38:43 -0700

    Add missing dependencty

--- manifests/init.pp
+++ manifests/init.pp
@@ -34,7 +34,7 @@ class uzblmonitor(
     ensure => purged,
   } ->
   # Install NoDM and Matchbox for kiosk-style display/window management
-  package { ['xserver-xorg', 'xserver-xorg-core', 'nodm', 'matchbox-window-manager']:
+  package { ['xserver-xorg', 'xserver-xorg-core', 'nodm', 'matchbox-window-manager', 'xnest']:
     ensure => latest,
   }
 
